dc45144e125da7705748c35bf2f14a7f8bbdffd0
Eugene Vihrev <evgeny.vihrev@gmail.com>
2022-03-19 18:26:54 +0300

    Create directories

--- .create_directories.py
+++ .create_directories.py
@@ -1,21 +1,31 @@
 import os
 
 
-with open('.students.txt', 'r') as f:
-    students = f.readlines()
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
 
-with open('.tasks.txt', 'r') as f:
-    tasks = f.readlines()
 
-paths = [f'./{s.lower().strip()}/{t.lower().strip()}' for s in students for t in tasks]
-new_paths = list(filter(lambda path: not os.path.exists(path), paths))
+def main():
+    with open('.students.txt', 'r') as f:
+        students = f.readlines()
 
-print('Following directories will be created')
-print(*new_paths, sep='\n')
-reply = input('Proceed? Y/N' + ' (y/n): ').lower().strip()
-if reply[:1] == 'y':
-    for path in new_paths:
-        os.makedirs(path)
-if reply[:1] == 'n':
-    pass
-print('Done')
+    with open('.tasks.txt', 'r') as f:
+        tasks = f.readlines()
+
+    paths = [f'./{s.lower().strip()}/{t.lower().strip()}' for s in students for t in tasks]
+    new_paths = list(filter(lambda path: not os.path.exists(path), paths))
+
+    print('Following directories will be created')
+    print(*new_paths, sep='\n')
+    reply = input('Proceed? Y/N' + ' (y/n): ').lower().strip()
+    if reply[:1] == 'y':
+        for path in new_paths:
+            os.makedirs(path)
+            touch(f'{path}/.placeholder')
+    if reply[:1] == 'n':
+        passp
+    print('Done')
+
+
+main()
